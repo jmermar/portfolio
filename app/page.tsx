e4edef5f93537dd1cf0c8bc021d9e405845288c4
Jose <josesmerono@gmail.com>
2026-01-19 23:51:11 +0100

    Created WIP splash

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import Header from "@/src/Header";
-import Hero from "@/src/Hero";
-import { Projects } from "@/src/Projects";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-5xl flex-col items-center  bg-white dark:bg-black sm:items-start">
-        <Header title="José Meroño Martínez" />
-        <Hero></Hero>
-        <Projects></Projects>
-      </main>
-    </div>
+    <>
+      <h1 className="font-mono text-2xl"> Work in progress.</h1>
+    </>
   );
 }
